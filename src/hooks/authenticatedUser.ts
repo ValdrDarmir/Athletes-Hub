08c3e366d81a6c6e746e54186145f2b74c6b827f
Shoggomo <andy-hot@web.de>
2023-07-21 14:21:57 +0200

    Fixed auth guard

--- src/hooks/authenticatedUser.ts
+++ src/hooks/authenticatedUser.ts
@@ -8,8 +8,10 @@ function useAuthenticatedUser() {
     const [authUser, authLoading, authError] = useAuthState(auth)
     const [dbUser, dbLoading, dbError] = useDocumentData(authUser ? doc(db.users, authUser.uid) : null)
 
+    const noDbUserError = authUser && !dbUser && new Error("User not found in database")
+
     const loading = authLoading || dbLoading
-    const error = authError || dbError
+    const error = authError || dbError || noDbUserError
 
     return [dbUser, loading, error] as const
 }
